87227ff312f0b4b4daa1c0f8cebac58c72542c14
Von'Erik Barren-Tucker <barrentucker.vonerik@gmail.com>
2022-03-04 09:57:44 -0500

    adding assets to portfolio

---
+++ admin/Components/01-ComponentTree/ComponentTree.js
@@ -0,0 +1,92 @@
+const IndexComponentTree = {
+   // Scene
+   RootReality: {
+      Title: "RootReality_Scene_L0",
+      Created: false,
+      Structure: {
+         Geometry: "",
+         Map: '',
+         Alpha: '',
+      },
+      Notes: [
+         "The scene I want to be very specific. Its going be a massive space where only certain things can be seen. The controls will be locked to a certain axis and only items can be viewed along that axis.",
+
+         'that being said, the following have to be met:',
+
+         'Thought: What if I had a torus as the wall(s)?'
+
+         [[false], 'Number of Walls for the Reality'],
+         [[false], 'dimensions of space'],
+         [[false], 'dimensions of user reality'],
+         [[false], 'wallpaper, feel, and atmospher of space'],
+         [[false], 'wallpaper, feel, and atmosphere of reality'],
+      ],
+   },
+
+   // Pointer Lock / Primary Controls / *Can be subject to change*
+   PointerLockReality: {
+      Title: "PointerLockRealityL0",
+      Created: false,
+      Structure: {
+         Geometry: "",
+         Map: '',
+         Alpha: '',
+      },
+      Notes: [
+         "With the controls, I want to have the user have the ability to look around. Not to have full control of the movement.",
+
+         [[false]'The interface I want to be accessible but I have to figure out how the pointerlock controls work. Start studing the controls.'],
+      ],
+   },
+
+   // Menu Reality => Start Menu (A menu available)
+   MenuReality: {
+      Title: "MenuRealityL0",
+      Created: false,
+      Structure: {
+         Geometry: "",
+         Map: '',
+         Alpha: '',
+      },
+      Notes: [
+         "",
+      ],
+   },
+
+
+   SideMenuReality: {
+      Title: "SideMenuRealityL0",
+      Created: false,
+      Structure: {
+         Geometry: "",
+         Map: '',
+         Alpha: '',
+      },
+      Notes: [
+         "",
+      ],
+   },
+
+
+   datGUIReality: {
+      Title: "datGUIRealityL0",
+      Created: false,
+      Structure: {
+         Geometry: "",
+         Map: '',
+         Alpha: '',
+      },
+      Notes: [
+         "",
+      ],
+   },
+
+
+   ComponentTreeDetails: {},
+
+
+
+
+
+
+}
---
+++ admin/THREE.notes/TimeReality/TimeReality.js
@@ -0,0 +1,9 @@
+const TimeReality = {
+   General: [
+      "General Notes",
+
+      '- The time reality will not be done ONLY with a tube, but also a torus. We will use the Torus capability to increase size and have realities programmed within the Toruses. Then Those Toruses will intersect with a given point. NOTE: The Toruses should always be grouped.',
+
+      '- We are working on the Portfolio now, but this is when I am going to come back to the time reality. The dat.GUI functionality should be enough to control the location of the realities across the timetubes'
+   ],
+}
